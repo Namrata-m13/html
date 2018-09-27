498b3fd54d629c4a465d51d790e2ee8ed696b3c7
Namrata Rajkumar Motwani <namratamotwani@Namratas-MacBook-Pro.local>
2018-09-27 19:19:27 +0530

    Js script

---
+++ jen_test.js
@@ -0,0 +1,15 @@
+var webdriver = require('selenium-webdriver'),
+By = webdriver.By;
+var assert=require('assert');
+var capabilities = {
+ 'browserName' : 'chrome'
+}
+var driver = new webdriver.Builder().usingServer('http://localhost:4444/wd/hub').withCapabilities(capabilities).build();
+driver.get('http://4af59c86.ngrok.io');
+driver.findElement(By.xpath("/html/body/p")).getText().then(function (ans){
+    if(assert.equal('Hello World',ans,"error2") === undefined){
+        console.log("Correct value");
+    } 
+});
+ 
+driver.quit();
