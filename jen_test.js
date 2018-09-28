f498082b68351a26712eb2c89190c7e4a1f4e060
Namrata Rajkumar Motwani <namratamotwani@Namratas-MacBook-Pro.local>
2018-09-28 12:55:13 +0530

    ngrok changes

--- jen_test.js
+++ jen_test.js
@@ -5,7 +5,7 @@ var capabilities = {
  'browserName' : 'chrome'
 }
 var driver = new webdriver.Builder().usingServer('http://localhost:4444/wd/hub').withCapabilities(capabilities).build();
-driver.get('http://2f18daf3.ngrok.io');
+driver.get('http://bc638997.ngrok.io');
 driver.findElement(By.xpath("/html/body/p")).getText().then(function (ans){
     if(assert.equal('Hello',ans,"error2") === undefined){
         console.log("Correct value");
