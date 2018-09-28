c36e61e6923c4d987c50c6302d8fedd9f6a2f530
Namrata Rajkumar Motwani <namratamotwani@Namratas-MacBook-Pro.local>
2018-09-28 12:02:52 +0530

    Changing the assert

--- jen_test.js
+++ jen_test.js
@@ -7,7 +7,7 @@ var capabilities = {
 var driver = new webdriver.Builder().usingServer('http://localhost:4444/wd/hub').withCapabilities(capabilities).build();
 driver.get('http://2f18daf3.ngrok.io');
 driver.findElement(By.xpath("/html/body/p")).getText().then(function (ans){
-    if(assert.equal('Hello World',ans,"error2") === undefined){
+    if(assert.equal('Hello',ans,"error2") === undefined){
         console.log("Correct value");
     } 
 });
